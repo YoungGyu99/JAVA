3a2fb4076a04c655dbfbf1437a71487bc582fca0
YoungGyu99 <147577156+YoungGyu99@users.noreply.github.com>
2023-12-19 22:52:52 +0900

    Add files via upload

---
+++ ch02/sec05/BooleanExample.java
@@ -0,0 +1,26 @@
+package ch02.sec05;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		
+		boolean stop = true;
+		if(stop) {
+			System.out.println("중지합니다");
+		}else {
+			System.out.println("시작합니다");
+		}
+		
+		int x = 10;
+		boolean result1 = (x == 20); // 변수 x의 값이 20인가?
+		boolean result2 = (x != 20); // 변수 x의 값이 20이 아닌가?
+		
+		// == 같은가?			= 다른가?
+		
+		
+		System.out.println("result1:" + result1);
+		System.out.println("result2:" + result2);
+		
+	}
+
+}
